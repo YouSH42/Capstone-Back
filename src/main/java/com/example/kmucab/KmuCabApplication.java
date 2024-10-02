2b7ad89ebb40b6223090d46e989082ed1851e338
김성재 <sungjve@gimseongjaeui-MacBookPro.local>
2024-10-02 16:30:26 +0900

    first commit

---
+++ src/main/java/com/example/kmucab/KmuCabApplication.java
@@ -0,0 +1,13 @@
+package com.example.kmucab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmuCabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KmuCabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kmucab/KmuCabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kmucab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KmuCabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
